8b363b5fc0b098923b10cd393bba92fbb49ef6bf
Tmacintyre <Tucker.w.macintyre@gmail.com>
2023-06-13 17:11:01 -0400

    Front-end Complete

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,20 +1,41 @@
-import React from "react"
+import React, {useState} from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+  const [name, setName] = useState("")
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const handleButtonClick = () => {
+    setModalOpen(true)
+  }
+
+  const handleModalClose = () => {
+    setModalOpen(false)
+  }
+
+  const handleReset = () => {
+    setName("")
+  }
+  
   return (
     <div className="entire-content">
-      <h1>Preparedness Assessment</h1>
+      <h1 class="header">Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
           <Label for="name">Enter your name</Label>
-          <Input />
+          <Input id="name" value={name} onChange={handleNameChange}/>
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <div class="button-container">
+        <Button onClick={handleButtonClick}>Click Me</Button>
+        <Button onClick={handleReset}>Reset</Button>
+        </div>
+        {modalOpen && <ModalComponent name={name} closeModal={handleModalClose} />}
       </div>
     </div>
   )
--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,24 @@
-import React from "react"
+import React from "react";
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
 
-const ModalComponent = () => {
-  return <></>
-}
+const ModalComponent = ({ name, closeModal }) => {
+  const handleModalClose = () => {
+    closeModal();
+  };
 
-export default ModalComponent
+  return (
+    <Modal isOpen={true} toggle={handleModalClose}>
+      <ModalHeader toggle={handleModalClose}>Hello, {name}!</ModalHeader>
+      <ModalBody>
+        <p>Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.</p>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="secondary" onClick={handleModalClose}>
+          Close
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default ModalComponent;
